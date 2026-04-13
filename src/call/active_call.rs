cfe2e28488524a2e4a06b04fdbf7ebda2a65c408
yeoleobun <yangli245@live.com>
2026-04-13 17:50:42 +0800

    fix: reject after ringing broke

--- src/call/active_call.rs
+++ src/call/active_call.rs
@@ -964,7 +964,22 @@ impl ActiveCall {
                 );
                 self.invitation.hangup(id, code, reason).await
             }
-            None => Ok(()),
+            None => {
+                let ready = self.call_state.write().await.ready_to_answer.take();
+                if let Some((_, _, dialog)) = ready {
+                    info!(
+                        session_id = self.session_id,
+                        ?reason,
+                        ?code,
+                        "rejecting call from ready_to_answer"
+                    );
+                    let dialog_id = dialog.id();
+                    dialog.reject(code, reason).ok();
+                    self.invitation.dialog_layer.remove_dialog(&dialog_id);
+                    self.cancel_token.cancel();
+                }
+                Ok(())
+            }
         }
     }
 
